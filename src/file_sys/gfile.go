7ecebfbf2fdb8c88bc703ceb475e74fed11bb4fa
zdglf <1042193998@qq.com>
2019-12-17 23:54:23 +0800

    add localfile

--- src/file_sys/gfile.go
+++ src/file_sys/gfile.go
@@ -5,23 +5,25 @@ import (
 	"regexp"
 )
 
+// General File 通用文件接口
 type GFile interface {
-	GetAbFilePath() string //获取文件绝对路径
-
-	GetFileSize() (int, error) //获取文件大小
-
-	GetFileName() string //货物文件名
-
-	Verify(username, password string) (bool, error) //验证账号密码
-
-	GetFileContent(hashAlgo string) (string, string, error) //返回文件hash Hex 值，和文件内容
-
-	IsDir() (bool, error) //是否文件目录
-
-	ListFile() ([]GFile, error) //列举文件
+	// 获取文件绝对路径
+	GetAbFilePath() string
+	// 获取文件大小
+	GetFileSize() (fileSize int, err error)
+	//获取文件名
+	GetFileName() (fileName string, err error)
+	// 验证账号密码
+	Verify(username, password string) (isVerify bool, err error)
+	// 返回文件hash Hex 值，和文件内容
+	GetFileContent() (hashValue string, content string, err error)
+	// 是否文件目录
+	IsDir() (isDir bool, err error)
+	// 列举文件
+	ListFile() (fileList []GFile, err error)
 }
 
-//是否文件大小超过限制
+// 是否文件大小超过限制
 func isOverSizeLimit(sizeLimit int, itemChild GFile) bool {
 
 	if sizeLimit >= 0 {
@@ -38,9 +40,15 @@ func isOverSizeLimit(sizeLimit int, itemChild GFile) bool {
 	}
 }
 
+// 是否匹配正则表达
 func isMatchString(re string, itemChild GFile) bool {
 	if re != "" {
-		if isOk, err := regexp.MatchString(re, itemChild.GetFileName()); err != nil {
+		var fileName string
+		var err error
+		if fileName,  err = itemChild.GetFileName();err!=nil{
+			return false
+		}
+		if isOk, err := regexp.MatchString(re, fileName); err != nil {
 			//正则异常跳过
 			fmt.Println(err.Error())
 			return false
@@ -54,10 +62,15 @@ func isMatchString(re string, itemChild GFile) bool {
 
 }
 
+// 遍历文件目录下的所有文件， 并调用 fileProcess 处理文件
 func WalkGFile(file GFile, chanSize int, re string, sizeLimit int, fileProcess func(f GFile) error) {
 	dirList := make([]GFile, 0, 100)
-	if file.IsDir() {
-		dirList = append(dirList, file)
+	if isDir, err := file.IsDir(); err != nil {
+		if isDir {
+			dirList = append(dirList, file)
+		}
+	} else {
+		fmt.Println(err.Error())
 	}
 	if chanSize <= 0 {
 		chanSize = 1
@@ -78,42 +91,56 @@ func WalkGFile(file GFile, chanSize int, re string, sizeLimit int, fileProcess f
 
 				fmt.Println(itemChild.GetFileName())
 
-				if itemChild.IsDir() {
-					dirList = append(dirList, itemChild)
+				if isDir, err := itemChild.IsDir(); err != nil {
+					fmt.Println(err.Error())
 				} else {
-					if isOverSizeLimit(sizeLimit, itemChild) {
-						continue
-					}
 
-					if !isMatchString(re, itemChild) {
-						continue
-					}
-
-				wait_loop:
-					for {
-						//文件处理协程大于
-						if len(buffList) >= cap(buffList) {
-							<-buffList
-							<-chanList
-						} else {
-							buffList <- 0
-							go func(c chan int, f GFile, fp func(f GFile) error) {
-								if err := fp(f); err != nil {
-									fmt.Println(err.Error())
-								}
-								<-c
-							}(chanList, itemChild, fileProcess)
-							break wait_loop
+					if isDir {
+						//是目录，添加到待处理列表中
+						dirList = append(dirList, itemChild)
+					} else {
+						//判断文件是否超过大小
+						if isOverSizeLimit(sizeLimit, itemChild) {
+							continue
 						}
-					}
+						//判断文件是否匹配正则表达式
+						if !isMatchString(re, itemChild) {
+							continue
+						}
 
+						wait_loop:
+						for {
+							//文件处理协程大于或等于，等待输出
+							if len(buffList) >= cap(buffList) {
+								<-buffList
+								<-chanList
+							} else {
+								//继续创建协程，处理文件
+								buffList <- 0
+								go func(c chan int, f GFile, fp func(f GFile) error) {
+									if err := fp(f); err != nil {
+										fmt.Println(err.Error())
+									}
+									<-c
+								}(chanList, itemChild, fileProcess)
+								break wait_loop
+							}
+						}
+
+					}
 				}
 			}
 		}
 	}
+	//等待处理完剩余协程
 	for i := 0; i < len(buffList); i++ {
 		<-buffList
 		<-chanList
 	}
 
+}
+
+// 解析文件内容
+func ParseFileContent(data []byte) string {
+	return string(data)
 }
--- src/file_sys/local_file.go
+++ src/file_sys/local_file.go
@@ -1,5 +1,110 @@
 package file_sys
 
+import (
+	"crypto"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+    "path"
+	"os"
+)
+
+var _ GFile = (*LocalFile)(nil)
+
 type LocalFile struct {
-    filePath string
+	filePath string
+}
+
+func (lf *LocalFile) GetAbFilePath() string {
+	return lf.filePath
+}
+
+func (lf *LocalFile) GetFileSize() (fileSize int, err error) {
+	var f *os.File
+	var fileInfo os.FileInfo
+
+	if f, err = os.Open(lf.filePath); err != nil {
+		return
+	}
+	defer f.Close()
+	if fileInfo, err = f.Stat(); err != nil {
+		return
+	}else{
+        fileSize = int(fileInfo.Size())
+    }
+	return
+}
+
+func (lf *LocalFile) GetFileName() (fileName string, err error) {
+	var f *os.File
+	var fileInfo os.FileInfo
+
+	if f, err = os.Open(lf.filePath); err != nil {
+		return
+	}
+	defer f.Close()
+	if fileInfo, err = f.Stat(); err == nil {
+        return
+	}else{
+        fileName = fileInfo.Name()
+    }
+	return
+}
+
+func (lf *LocalFile) Verify(username, password string) (isVerify bool, err error) {
+	return true, nil
+}
+
+func (lf *LocalFile) GetFileContent() (hashValue string, content string, err error) {
+	var f *os.File
+	if f, err = os.Open(lf.filePath); err != nil {
+		return
+	}
+	defer f.Close()
+	var data []byte
+	if data, err = ioutil.ReadAll(f); err != nil {
+		return
+	}
+	var hash = crypto.MD5.New()
+	hashValue = hex.EncodeToString(hash.Sum(data))
+	content = ParseFileContent(data)
+	return
+
+}
+
+func (lf *LocalFile) IsDir() (isDir bool, err error) {
+
+	var f *os.File
+	var fileInfo os.FileInfo
+	isDir = false
+    fmt.Println(lf.filePath)
+	if f, err = os.Open(lf.filePath); err != nil {
+		return
+	}
+	defer f.Close()
+	if fileInfo, err = f.Stat(); err != nil {
+		return
+	}else{
+        isDir = fileInfo.IsDir()
+    }
+	return
+
+}
+
+func (lf *LocalFile) ListFile() (fileList []GFile, err error) {
+	var f *os.File
+	var childNameList []string
+	if f, err = os.Open(lf.filePath); err != nil {
+		return
+	}
+	defer f.Close()
+	if childNameList, err = f.Readdirnames(0); err != nil {
+        return
+	}else{
+        for _, childName := range childNameList {
+            fileList = append(fileList, &LocalFile{path.Join(lf.filePath,childName)})
+        }
+    }
+	return
+
 }
